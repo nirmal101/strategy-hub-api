fbeea10a2f6b90f46d8f80b4def5effb8b640e26
Dhanushka Nirmal <dhanushkahere@gmail.com>
2026-03-15 11:18:28 +1100

    SH-5 Intake Service

--- src/main/java/com/strategyhub/creator/repository/CreatorProfileRepository.java
+++ src/main/java/com/strategyhub/creator/repository/CreatorProfileRepository.java
@@ -1,9 +1,9 @@
 package com.strategyhub.creator.repository;
 
-import com.strategyhub.creator.entity.CreatorProfile;
+import com.strategyhub.creator.entity.CreatorProfileEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface CreatorProfileRepository extends JpaRepository<CreatorProfile, UUID> {
+public interface CreatorProfileRepository extends JpaRepository<CreatorProfileEntity, UUID> {
 }
--- src/main/java/com/strategyhub/creator/repository/CreatorSocialAccountRepository.java
+++ src/main/java/com/strategyhub/creator/repository/CreatorSocialAccountRepository.java
@@ -1,9 +1,9 @@
 package com.strategyhub.creator.repository;
 
-import com.strategyhub.creator.entity.CreatorSocialAccount;
+import com.strategyhub.creator.entity.CreatorSocialAccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface CreatorSocialAccountRepository extends JpaRepository<CreatorSocialAccount, UUID> {
+public interface CreatorSocialAccountRepository extends JpaRepository<CreatorSocialAccountEntity, UUID> {
 }
